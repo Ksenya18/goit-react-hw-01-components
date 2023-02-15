691e38d70cb33f668015a1c175febfff62e45b75
Ksenya18 <106611808+Ksenya18@users.noreply.github.com>
2023-02-15 18:11:52 +0200

    components updated

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -24,6 +24,6 @@ FriendListItem.propTypes = {
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-    })
-  ),
+    }).isRequired
+  ).isRequired,
 };
